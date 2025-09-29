a21c875a8d560a992198cc24ad8f4f6c3c66c454
XBanTs <ayomideoyewo@gmail.com>
2025-09-29 19:02:29 +0100

    Initial commit

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://recommendation-api-3wqy.onrender.com/"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
 
